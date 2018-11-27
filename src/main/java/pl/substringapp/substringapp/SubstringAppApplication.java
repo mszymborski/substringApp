43b47b32ed4ce159541ef04f356732ba86c37b3a
mar14 <19radiogra@gmail.com>
2018-11-27 19:48:38 +0100

    Initial Commit

---
+++ src/main/java/pl/substringapp/substringapp/SubstringAppApplication.java
@@ -0,0 +1,12 @@
+package pl.substringapp.substringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubstringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubstringAppApplication.class, args);
+	}
+}
